d921ddd09121e3e73a05c6ff31dfdd1b8f2657b1
vlad-tokarev799 <vlad.tokarev.200247@gmail.com>
2021-12-14 03:12:00 +0800

    add header

---
+++ app/js/script.js
@@ -0,0 +1,13 @@
+$(() => {
+    $("#topNavBurger").click(e => {
+        $('#topNavList').toggleClass('top-nav__list_open')
+        $('#overlay').toggleClass('overlay_open')
+
+        return false
+    })
+
+    $("#overlay").click(e => {
+        $('#overlay').removeClass('overlay_open')
+        $('#topNavList').removeClass('top-nav__list_open')
+    })
+})
